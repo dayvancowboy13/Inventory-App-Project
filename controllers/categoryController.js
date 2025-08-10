698c75181fda9671ad275083bcd988dcce75a1fd
dayvancowboy13 <chris.e.friedrich@gmail.com>
2025-08-10 10:14:45 -0700

    Add validation to new category submission

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,22 +1,50 @@
 const db = require('../db/queries');
+const { body, validationResult } = require("express-validator");
 
-exports.get = async (req, res) => {
+const validateCategory = [
+    body('categoryName').trim().isLength({ min: 1, max: 30 })
+        .withMessage('Category name must be between 1 and 30 characters')
+        .isAlphanumeric().withMessage('Category name can only contain letters or numbers'),
+    body('categoryDescription').trim().isLength({ max: 200 })
+        .withMessage('Category description must contain fewer than 200 characters')
+];
+
+exports.getCategoryPage = async (req, res) => {
     let content = await db.getCategories();
-    console.log(content);
     res.render('categories', { categories: content });
 };
 
-exports.addCategory = async (req, res) => {
+exports.getAddCategoryPage = async (req, res) => {
     res.render('addCategory');
 };
 
-exports.post = async (req, res) => {
-    await db.addCategory(
-        req.body.categoryName,
-        req.body.categoryDescription,
-    )
-    res.redirect('./');
-};
+// exports.post = async (req, res) => {
+//     await db.addCategory(
+//         req.body.categoryName,
+//         req.body.categoryDescription,
+//     )
+//     res.redirect('./');
+// };
+
+exports.postNewCategory = [
+    validateCategory, async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render('addCategory',
+                {
+                    errors: errors.array()
+                }
+            )
+        } else {
+            const { categoryName, categoryDescription } = req.body;
+            await db.addCategory(
+                categoryName,
+                categoryDescription,
+            )
+            res.redirect('./');
+        }
+    }
+]
 
 exports.getEditPage = async (req, res) => {
     console.log('sending the edit page')
@@ -25,7 +53,7 @@ exports.getEditPage = async (req, res) => {
     res.render('editCategory', { catInfo });
 };
 
-exports.editCategory = async (req, res) => {
+exports.postEditCategory = async (req, res) => {
     const id = Number(req.url.split('/')[2]);
     await db.updateCategory(
         id,
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -3,11 +3,10 @@ const categoryController = require('../controllers/categoryController')
 
 const categoryRouter = Router();
 
-categoryRouter.get('/', categoryController.get);
-categoryRouter.get('/add', categoryController.addCategory);
+categoryRouter.get('/', categoryController.getCategoryPage);
+categoryRouter.get('/add', categoryController.getAddCategoryPage);
 categoryRouter.get('/edit/:id', categoryController.getEditPage);
-categoryRouter.post('/add', categoryController.post);
-categoryRouter.post('/edit/:id', categoryController.editCategory);
-
+categoryRouter.post('/add', categoryController.postNewCategory);
+categoryRouter.post('/edit/:id', categoryController.postEditCategory);
 
 module.exports = categoryRouter;

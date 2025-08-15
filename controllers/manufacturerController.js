de0b6d0d2a846501577e0acbd5bd84b3b5bd2431
dayvancowboy13 <chris.e.friedrich@gmail.com>
2025-08-14 19:26:26 -0700

    Add form validation to manufacturer pages

--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -1,25 +1,46 @@
 const db = require('../db/queries');
 const { body, validationResult } = require("express-validator");
 
-exports.get = async (req, res) => {
+const validateManufacturer = [
+    body('manufacturerName').trim().isLength({ min: 1, max: 30 })
+        .withMessage('Manufacturer name must be between 1 and 30 characters')
+        .matches(/^[a-zA-Z0-9 ]+$/).withMessage('Product name can only contain letters, numbers, or spaces'),
+    body('manufacturerNotes').trim().isLength({ max: 200 })
+        .withMessage('Manufacturer notes must contain fewer than 200 characters'),
+    body('manufacturerLocation').trim().isLength({ min: 1, max: 30 })
+        .withMessage('Manufacturer location must be between 1 and 30 characters')
+        .matches(/^[a-zA-Z0-9 ]+$/).withMessage('Product location can only contain letters, numbers, or spaces')
+];
+
+exports.getManufacturerPage = async (req, res) => {
     let content = await db.getManufacturers();
     res.render('manufacturers', { manufacturers: content });
 };
 
-exports.addManufacturer = async (req, res) => {
+exports.getAddManufacturerPage = async (req, res) => {
     res.render('addManufacturer');
 }
 
-exports.post = async (req, res) => {
-    console.log('adding new manufacturer...')
-    await db.addManufacturer(
-        req.body.manufacturerName,
-        req.body.manufacturerLocation,
-        req.body.manufacturerNotes,
-    );
-    console.log('done with the important stuff, trying redirect...')
-    res.redirect('./');
-}
+exports.postNewManufacturer = [
+    validateManufacturer, async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).render('addManufacturer',
+                {
+                    errors: errors.array()
+                }
+            )
+        } else {
+            console.log('adding new manufacturer...')
+            await db.addManufacturer(
+                req.body.manufacturerName,
+                req.body.manufacturerLocation,
+                req.body.manufacturerNotes,
+            );
+            res.redirect('./');
+        }
+    }]
 
 exports.getEditPage = async (req, res) => {
     console.log('sending the edit page')
@@ -28,13 +49,28 @@ exports.getEditPage = async (req, res) => {
     res.render('editManufacturer', { manInfo });
 };
 
-exports.editManufacturer = async (req, res) => {
-    const id = Number(req.url.split('/')[2]);
-    await db.updateManufacturer(
-        id,
-        req.body.manufacturerName,
-        req.body.manufacturerLocation,
-        req.body.manufacturerNotes
-    );
-    res.redirect('/manufacturers');
-}
+exports.postEditManufacturer = [
+    validateManufacturer, async (req, res) => {
+        const id = Number(req.url.split('/')[2]);
+        const errors = validationResult(req);
+        const manInfo = await db.getManufacturerById(id);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).render('editManufacturer',
+                {
+                    errors: errors.array(),
+                    manInfo
+                }
+            )
+        } else {
+
+            await db.updateManufacturer(
+                id,
+                req.body.manufacturerName,
+                req.body.manufacturerLocation,
+                req.body.manufacturerNotes
+            );
+            res.redirect('/manufacturers');
+        }
+
+    }]
--- routes/manufacturerRouter.js
+++ routes/manufacturerRouter.js
@@ -3,10 +3,10 @@ const manufacturerController = require('../controllers/manufacturerController')
 
 const manufacturerRouter = Router();
 
-manufacturerRouter.get('/', manufacturerController.get);
-manufacturerRouter.get('/add', manufacturerController.addManufacturer);
+manufacturerRouter.get('/', manufacturerController.getManufacturerPage);
+manufacturerRouter.get('/add', manufacturerController.getAddManufacturerPage);
 manufacturerRouter.get('/edit/:id', manufacturerController.getEditPage);
-manufacturerRouter.post('/edit/:id', manufacturerController.editManufacturer);
-manufacturerRouter.post('/add', manufacturerController.post);
+manufacturerRouter.post('/add', manufacturerController.postNewManufacturer);
+manufacturerRouter.post('/edit/:id', manufacturerController.postEditManufacturer);
 
 module.exports = manufacturerRouter;

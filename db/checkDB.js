642e24cdcf3cfd6be222fb54cb170f1adcab5e23
dayvancowboy13 <chris.e.friedrich@gmail.com>
2025-08-17 09:51:23 -0700

    Await database functions during check phase

--- db/checkDB.js
+++ db/checkDB.js
@@ -3,13 +3,13 @@ const dbFuncs = require('../db/populatedb');
 
 exports.checkDB = async function () {
     try {
-        db.checkDB();
+        await db.checkDB();
     } catch (error) {
         console.log(error);
 
         try {
-            dbFuncs.init();
-            dbFuncs.populate();
+            await dbFuncs.init();
+            await dbFuncs.populate();
         } catch (error) {
             console.log(error);
         }
